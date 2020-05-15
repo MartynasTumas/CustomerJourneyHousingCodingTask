ecd031f37882764d02d7010b2a82ef0cbc7470b5
Martynas <martynas.tumas@gmail.com>
2020-05-15 19:09:32 +0300

    Insert new tax to db and insert new tax from file

--- CustomerJourneyHousingCodingTask/TaxCalculationService/Program.cs
+++ CustomerJourneyHousingCodingTask/TaxCalculationService/Program.cs
@@ -1,12 +1,119 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TaxCalculationService
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string connectionString = @"Server=DESKTOP-4DG3M6E\SQLEXPRESS; Database = MunicipalityTax; Trusted_Connection=true;";
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            //  InsertTaxesFromFile();
+
+
+        }
+
+        public static void InsertTaxesFromFile()
+        {
+            DataTable dataFromFile = GetDataTableFromCSVFile("TaxData.csv");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlBulkCopy s = new SqlBulkCopy(connection))
+                    {
+                        s.DestinationTableName = "Tax";
+                        foreach (var column in dataFromFile.Columns)
+                            s.ColumnMappings.Add(column.ToString(), column.ToString());
+                        s.WriteToServer(dataFromFile);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection. " + ex.Message);
+            }
+        }
+
+        public static void InsertNewTax(string municipality, double tax, DateTime startDate, DateTime endDate)
+        {
+            if (string.IsNullOrEmpty(municipality))
+            {
+                Console.WriteLine("Error. Municipality is not provided");
+            }
+            else if (tax == 0)
+            {
+                Console.WriteLine("Error. Tax is not provided");
+            }
+            else
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        string commandText = "INSERT INTO Tax (Municipality, Tax, StartDate, EndDate) VALUES (@Municipality, @Tax, @StartDate, @EndDate)";
+                        command.Connection = connection;
+                        command.CommandText = commandText;
+                        command.Parameters.AddWithValue("@Municipality", municipality);
+                        command.Parameters.AddWithValue("@Tax", tax);
+                        command.Parameters.AddWithValue("@StartDate", startDate);
+                        command.Parameters.AddWithValue("@EndDate", endDate);
+                        try
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (SqlException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static DataTable GetDataTableFromCSVFile(string filePath)
+        {
+            DataTable fileData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(filePath))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        fileData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        //Making empty value as null
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        fileData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading CSV file. " + ex.Message);
+                return null;
+            }
+            return fileData;
         }
     }
 }
---
+++ CustomerJourneyHousingCodingTask/TaxCalculationService/TaxModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculationService
+{
+    public class TaxModel
+    {
+        private string City { get; set; }
+        private double Tax { get; set; }
+        private DateTime StartDate { get; set; }
+        private DateTime EndDate { get; set; }
+    }
+}

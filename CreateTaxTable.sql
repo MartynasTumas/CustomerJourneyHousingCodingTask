3f7eda4c30f6a930fcb34d3784caf5daa4a4b2df
Martynas <martynas.tumas@gmail.com>
2020-05-18 15:53:37 +0300

    Updated CreateTaxTable

--- CreateTaxTable.sql
+++ CreateTaxTable.sql
@@ -1,7 +1,7 @@
 USE [MunicipalityTax]
 GO
 
-/****** Object:  Table [dbo].[Tax]    Script Date: 2020-05-18 12:08:23 ******/
+/****** Object:  Table [dbo].[Tax]    Script Date: 2020-05-18 15:53:15 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,12 +9,16 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Tax](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[Municipality] [nvarchar](50) NOT NULL,
 	[TaxAmount] [float] NOT NULL,
+	[Type] [nvarchar](50) NOT NULL,
 	[StartDate] [datetime] NOT NULL,
 	[EndDate] [datetime] NULL,
-	[Type] [nvarchar](50) NOT NULL
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
 

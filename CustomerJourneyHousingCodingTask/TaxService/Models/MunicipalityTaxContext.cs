c7773fc472952642a36207e4d201b5668efd834e
Martynas <martynas.tumas@gmail.com>
2020-05-18 10:28:29 +0300

    Removed console projects and added API project

---
+++ CustomerJourneyHousingCodingTask/TaxService/Models/MunicipalityTaxContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TaxService.Models
+{
+    public partial class MunicipalityTaxContext : DbContext
+    {
+        public MunicipalityTaxContext()
+        {
+        }
+
+        public MunicipalityTaxContext(DbContextOptions<MunicipalityTaxContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tax> Tax { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4DG3M6E\\SQLEXPRESS; Database = MunicipalityTax; Trusted_Connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tax>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.EndDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Municipality)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.StartDate).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CustomerJourneyHousingCodingTask/TaxService/Models/Tax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaxService.Models
+{
+    public partial class Tax
+    {
+        public string Municipality { get; set; }
+        public double TaxAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
